7a4cd777be9e1b3b641d16ac5f40a22ee93249e6
voro282 <43708321+Zedzeen@users.noreply.github.com>
2021-11-21 20:29:44 +0100

    Added basic item detection to the system including visual feedback by changing the color of the shader.

---
+++ Assets/Scripts/SHOP_ItemDetection.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SHOP_ItemDetection : MonoBehaviour
+{
+    // Due to now knowing in what oder the items are removed in, the GameObject is both the reference and the value. 
+    Dictionary<GameObject,GameObject> items = new Dictionary<GameObject, GameObject>(); 
+
+    Material m_Material;
+
+    [SerializeField]
+    Color hasItemsColor = Color.green;
+   
+    [SerializeField]
+    Color emptyColor = Color.red;
+
+    private void Start() {
+        m_Material = GetComponent<MeshRenderer>().sharedMaterial;
+        m_Material.SetColor("_Color", emptyColor);
+
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        items.Add(other.gameObject, other.gameObject);
+        
+        if (items.Count != 0)
+            m_Material.SetColor("_Color", hasItemsColor);
+    }
+
+    private void OnTriggerExit(Collider other) {
+        items.Remove(other.gameObject);
+
+        if (items.Count == 0)
+            m_Material.SetColor("_Color", emptyColor);
+    }
+}
