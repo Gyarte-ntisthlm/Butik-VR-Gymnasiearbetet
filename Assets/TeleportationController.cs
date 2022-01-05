35fc453b6138a073c1a730016301745a77a3da9e
voro282 <43708321+Zedzeen@users.noreply.github.com>
2022-01-05 15:19:43 +0100

    Added new project

---
+++ Assets/TeleportationController.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Events;
+using System;
+
+public class TeleportationController : MonoBehaviour
+{
+  public GameObject baseControllerGameObject;
+  public GameObject teleportationGameObject;
+
+  public InputActionReference teleportActionRef;
+  
+  [Space]
+  public UnityEvent onTeleportActivate;
+  public UnityEvent onTeleportCancel;
+
+
+  private void Start()
+  {
+    teleportActionRef.action.performed += TeleportActivate;
+    teleportActionRef.action.canceled += TeleportCancel;
+  }
+
+  private void TeleportActivate(InputAction.CallbackContext obj)
+  {
+    onTeleportActivate.Invoke();
+  }
+
+  private void TeleportCancel(InputAction.CallbackContext obj)
+  {
+      Debug.Log("TeleportCancel");
+    Invoke("DeactivateTeleportation", 0.1f);
+  }
+
+  private void DeactivateTeleportation()
+  {
+    onTeleportCancel.Invoke();
+  }
+}
