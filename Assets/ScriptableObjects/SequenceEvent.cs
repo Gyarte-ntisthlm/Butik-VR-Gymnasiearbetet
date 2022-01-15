2c0c9b3ff176c758c757340b40f1f5df87b8ae83
voro282 <43708321+Zedzeen@users.noreply.github.com>
2022-01-15 12:14:45 +0100

    Added more logic to the sequences

--- Assets/ScriptableObjects/SequenceEvent.cs
+++ Assets/ScriptableObjects/SequenceEvent.cs
@@ -1,13 +1,39 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 [Serializable]
 [CreateAssetMenu(menuName = "Events/SequenceEvent")]
 public class SequenceEvent : ScriptableObject
-{
-    public string subtitle;
+{ 
+    [Tooltip("Usefull if you want to let the player to \"take in\" their surroundings before playing the audio etc.")]
+    [Range(0, 30)]
     public float delayFromTrigger;
+    
+    [Tooltip("If the light should be activated at a special point in the sequence. For instance on a specific sentence in the audio clip.")]
+    [Range(0, 30)]
+    public float delayLightActivation;
+    
+    [Tooltip("If the objects should be activated at a special point in the sequence. For instance on a specific sentence in the audio clip.")]
+    [Range(0, 30)]
+    public float delayObjectActivation;
+
+    [Tooltip("This should be a bit longer than the audio clip duration.")]
+    [Range(0, 30)]
     public float subtitleDuration;
-    public AudioSource audioSource;
+
+    [Space]
+
     public AudioClip audioClip;
+    public string subtitle;
+
+    [Space]
+
+    public AudioSource audioSource;
+    public Light light;
+    public List<GameObject> objectsToActivate;
+
+    [Space]
+    [Tooltip("GameManager events to trigger. If left empty will not trigger any events.")]
+    public List<string> eventsToTrigger;
 }
--- Assets/Scripts/SequenceManager.cs
+++ Assets/Scripts/SequenceManager.cs
@@ -1,8 +1,5 @@
-using System;
-using System.Collections.Generic;
 using System.Collections;
 using UnityEngine;
-using UnityEngine.Events;
 
 public class SequenceManager : MonoBehaviour
 {
@@ -11,7 +8,6 @@ public class SequenceManager : MonoBehaviour
     private void Awake() {
         instance = this;
     }
-
 
     public void OnEventTriggered(SequenceEvent sequenceEvent)
     {
@@ -25,6 +21,12 @@ public class SequenceManager : MonoBehaviour
     {
         // Wait for delay.
         yield return new WaitForSeconds(sequenceEvent.delayFromTrigger);
+
+        // Activate light.
+        StartCoroutine(ActivateLight(sequenceEvent));
+
+        // Activate objects.
+        StartCoroutine(ActivateGameObjects(sequenceEvent));
 
         // Play the audio clip.
         if (sequenceEvent.audioSource != null && sequenceEvent.audioClip != null)
@@ -45,6 +47,41 @@ public class SequenceManager : MonoBehaviour
         SubtitleManager.instance.HideSubtitle();
 
 
+        // If there are any game manager events to trigger, trigger them.
+        foreach (string eventToTrigger in sequenceEvent.eventsToTrigger)
+        {
+            if (!string.IsNullOrEmpty(eventToTrigger))
+            {
+                Invoke($"GameManger.instance.{sequenceEvent.eventsToTrigger}()", 0);
+            }
+        }
+    }
+
+    IEnumerator ActivateLight(SequenceEvent sequenceEvent)
+    {
+        // Wait for delay.
+        yield return new WaitForSeconds(sequenceEvent.delayLightActivation);
+
+        // Turn on the lights.
+        if (sequenceEvent.light != null)
+        {
+            sequenceEvent.light.enabled = true;
+        }
+    }
+
+    IEnumerator ActivateGameObjects(SequenceEvent sequenceEvent)
+    {
+        // Wait for delay.
+        yield return new WaitForSeconds(sequenceEvent.delayObjectActivation);
+
+        // Turn on the objects.
+        if (sequenceEvent.objectsToActivate != null)
+        {
+            foreach (GameObject go in sequenceEvent.objectsToActivate)
+            {
+                go.SetActive(true);
+            }
+        }
     }
 
     
