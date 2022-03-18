9b2e5cf2783685695ab7da3a974fd76f691cfd02
Zedzee <43708321+Zedzeen@users.noreply.github.com>
2022-03-18 08:19:35 +0100

    animations n stuff

---
+++ Assets/CashregisterController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CashregisterController : MonoBehaviour
+{
+    Animator animator;
+
+    // Register to the purchase completed event from the game manager
+    void Start()
+    {
+        GameManager.instance.onPurchaseCompleted += OnPurchaseCompleted;
+        GameManager.instance.onPurchaseBegin += OnPurchaseBegin;
+
+        animator = GetComponent<Animator>();
+    }
+
+    // When the purchase is completed, play the "Done" animation clip from the cash register
+    void OnPurchaseCompleted()
+    {
+        animator.Play("Armature|Done");
+    }
+
+    void OnPurchaseBegin()
+    {
+        animator.Play("Armature|AddedItem");
+    }
+}
