96339574f001f5d0d3c4f35f5fe701f38efc8124
Zedzee <43708321+Zedzeen@users.noreply.github.com>
2022-03-24 14:54:33 +0100

    Made the buttons not use a "Console" and re added the old door.

--- Assets/DoorController.cs
+++ Assets/DoorController.cs
@@ -1,18 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Events;
 using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine;
 
 public class DoorController : XRGrabInteractable
 {
-    public UnityEvent OnDoorOpen;
 
     // This is called when the player grabs the doorknob.
     protected override void OnSelectEntering(SelectEnterEventArgs args)
     {
         base.OnSelectEntering(args);
         
-        OnDoorOpen.Invoke();
+        GameManager.instance.OnEvalCompleted();
     }
 }
