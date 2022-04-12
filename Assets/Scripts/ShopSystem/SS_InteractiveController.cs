ced54a0c7a4a8371ccf6fffe0efe159305afc89b
Zedzee <43708321+Zedzeen@users.noreply.github.com>
2022-04-12 03:00:14 +0200

    Clean up, Interactive test scene almost done.

--- Assets/Scripts/ShopSystem/SS_InteractiveController.cs
+++ Assets/Scripts/ShopSystem/SS_InteractiveController.cs
@@ -1,36 +1,21 @@
 using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
 
 public class SS_InteractiveController : MonoBehaviour
 {
-    // private List<GameObject> purchaseObjects;
-
-    // private void OnTriggerEnter(Collider other)
-    // {
-    //     // Check if the other has the tag of item and also check if there is a gameobject with the tag of Coin contained in the purchaseObjects list.
-    //     if (other.gameObject.tag != "Item") return;
-
-    //     // Add the other gameobject to the purchaseObjects list.
-    //     purchaseObjects.Add(other.gameObject);
-
-    //     // Begin the purchase process.
-    //     GameManager.instance.OnPurchaseBegin();
-
-    //     // Check if the purchaseObjects list contains a gameobject with the tag of Coin using a for loop.
-    //     for (int i = 0; i < purchaseObjects.Count; i++)
-    //     {
-    //         Debug.Log(purchaseObjects[i].tag);
-
-    //         if (purchaseObjects[i].tag == "Coin")
-    //         {
-    //             // If the purchaseObjects list contains a gameobject with the tag of Coin, then call the OnPurchase event.
-    //             GameManager.instance.OnPurchaseCompleted();
-    //         }
-    //     }
-    // }
-
-    private List<GameObject> coins = new List<GameObject>();
+    private bool hasMoney = false;
     private List<GameObject> purchaseObjects = new List<GameObject>();
+
+    public void AddMoney()
+    {
+        hasMoney = true;
+    }
+
+    public void RemoveMoney()
+    {
+        hasMoney = false;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -40,14 +25,16 @@ public class SS_InteractiveController : MonoBehaviour
                 GameManager.instance.OnPurchaseBegin();
 
             purchaseObjects.Add(other.gameObject);
-        }
-        else if (other.gameObject.tag == "Coin")
-        {
-            coins.Add(other.gameObject);
+            print("Added " + other.gameObject.name + " to purchaseObjects");
         }
 
-        if (other.gameObject.tag == "Coin" && purchaseObjects.Count > 0)
-            GameManager.instance.OnPurchaseCompleted();
+        if (hasMoney && purchaseObjects.Count > 0) StartCoroutine(Purchase());
+    }
+
+    IEnumerator Purchase()
+    {
+        yield return new WaitForSeconds(2f);
+        GameManager.instance.OnPurchaseCompleted();
     }
 
     private void OnTriggerExit(Collider other)
@@ -58,10 +45,6 @@ public class SS_InteractiveController : MonoBehaviour
 
             if (purchaseObjects.Count == 0)
                 GameManager.instance.OnPurchaseAborted();
-        }
-        else if (other.gameObject.tag == "Coin")
-        {
-            coins.Remove(other.gameObject);
         }
     }
 }
