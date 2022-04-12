fa8f554e8eaf3ec05eee99aa3804f0f0dcdcad5c
Zedzee <43708321+Zedzeen@users.noreply.github.com>
2022-04-12 23:06:21 +0200

    Interactive scene complete

--- Assets/Scripts/ShopSystem/SS_InteractiveController.cs
+++ Assets/Scripts/ShopSystem/SS_InteractiveController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SS_InteractiveController : MonoBehaviour
 {
     private bool hasMoney = false;
+    private bool isInvoked = false;
     private List<GameObject> purchaseObjects = new List<GameObject>();
 
     public void AddMoney()
@@ -28,12 +29,19 @@ public class SS_InteractiveController : MonoBehaviour
             print("Added " + other.gameObject.name + " to purchaseObjects");
         }
 
+    }
+
+
+    public void InvokePurchase()
+    {
+        if(isInvoked) return; // Don't invoke twice.
+        isInvoked = true;
         if (hasMoney && purchaseObjects.Count > 0) StartCoroutine(Purchase());
     }
 
     IEnumerator Purchase()
     {
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(10f);
         GameManager.instance.OnPurchaseCompleted();
     }
 
